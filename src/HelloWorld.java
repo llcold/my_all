b6dd762eca0312de8d1888e24c566b2352f4e3f0
llcold <576313573@QQ.com>
2018-09-15 23:26:54 +0800

    haha

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//System.out.println("Hello World!");
+		String item=new String();
+		System.out.println(item.equals(""));
+	}
+}
